2ae737eac87aca5c20d27dfc91484a9e4e81e8c3
AlexMSm <alexsmithuom@gmail.com>
2022-10-11 22:58:00 +0800

    add getCommentsByArticleId functionality with error handling for faulty id, unmatched id and matched id with no comments

--- _models/articles.models.js
+++ _models/articles.models.js
@@ -103,3 +103,28 @@ exports.selectArticlesByTopic = async (topic) => {
     }
   });
 };
+
+exports.selectCommentsByArticleId = async (article_id) => {
+  const article = await this.selectArticleById(article_id);
+  if (article.error) {
+    return article;
+  } else {
+    return db
+      .query(
+        `SELECT comments.* FROM comments
+    LEFT JOIN articles ON articles.article_id = comments.article_id WHERE comments.article_id = $1 ORDER BY created_at DESC;`,
+        [article_id]
+      )
+      .then((response) => {
+        if (response.rows.length === 0) {
+          return Promise.reject({
+            error: true,
+            status: 404,
+            msg: `No comments found.`,
+          });
+        } else {
+          return response.rows;
+        }
+      });
+  }
+};
