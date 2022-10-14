92ae1ca8a088c0fde7afc0c06f6c727e20c5681b
AlexMSm <alexsmithuom@gmail.com>
2022-10-14 09:49:07 +0800

    Heroku configurations added 3

--- listen.js
+++ listen.js
@@ -3,8 +3,6 @@ const app = require("./app");
 
 const express = require("express");
 
-const { PORT = 9090 } = process.env;
-
 app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`Listening on ${PORT}...`);
