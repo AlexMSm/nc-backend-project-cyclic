79f58bcfef19825359e31cd1195f3c1e1f767e33
AlexMSm <alexsmithuom@gmail.com>
2022-10-16 05:28:03 +0800

    Small change to api router

--- routes/api-router.js
+++ routes/api-router.js
@@ -4,14 +4,14 @@ const topicsRouter = require('./topics-router');
 const articlesRouter = require('./articles-router');
 const commentsRouter = require('./comments-router');
 
+apiRouter.get("/", (req, res) => {
+  const endpoint = require("./endpoints.json");
+  res.status(200).send({ endpoint });
+});
+
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/articles', articlesRouter);
 apiRouter.use('/comments', commentsRouter);
-
-apiRouter.get("/", (req, res) => {
-    const endpoint = require("./endpoints.json");
-    res.status(200).send({ endpoint });
-  });
 
 module.exports = apiRouter;
