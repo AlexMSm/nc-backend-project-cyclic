59a2be11b932b80b15d1a8d2ef0e86385ae9bb6f
AlexMSm <alexsmithuom@gmail.com>
2022-10-31 10:09:10 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
-const apiRouter = require('./routes/api-router');
+const apiRouter = require("./routes/api-router");
 
-app.use('/api', apiRouter);
-
+app.use("/api", apiRouter);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ message: "Bad path" });
