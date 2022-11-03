c0dafb4ef1e9b8f21f8f123beceb68adf4d1d553
AlexMSm <alexsmithuom@gmail.com>
2022-11-03 14:24:17 +0000

    api router changes for article comment posting

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -1,18 +1,17 @@
 const {
-    getArticleById,
-    patchVoteById,
-    getArticles,
-    postArticle,
-    deleteArticle
-  } = require("../_controllers/articles.controllers");
+  getArticleById,
+  patchVoteById,
+  getArticles,
+  postArticle,
+  deleteArticle,
+} = require("../_controllers/articles.controllers");
 
-  const {
-    getCommentsByArticleId,
-    postCommentToArticle,
-    
-  } = require("../_controllers/comments.controllers");
+const {
+  getCommentsByArticleId,
+  postCommentToArticle,
+} = require("../_controllers/comments.controllers");
 
-const articlesRouter = require('express').Router();
+const articlesRouter = require("express").Router();
 
 articlesRouter.get("/", getArticles);
 articlesRouter.get("/:article_id", getArticleById);
@@ -21,7 +20,6 @@ articlesRouter.patch("/:article_id", patchVoteById);
 articlesRouter.get("/:article_id/comments", getCommentsByArticleId);
 articlesRouter.post("/", postArticle);
 articlesRouter.post("/:article_id/comments", postCommentToArticle);
-articlesRouter.delete('/:article_id', deleteArticle)
-
+articlesRouter.delete("/:article_id", deleteArticle);
 
 module.exports = articlesRouter;
